6f91bfbb96ef5a4896482ba5bcd5fb5d8888bd4b
Zain Dewsi <zain.dewsi@gmail.com>
2021-04-21 14:16:56 -0400

    Main CTA page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,21 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
 const Login = (props) => {
   return (
-  <Container>
-    <Content>
-      Content
-    </Content>
-  </Container>
-  )
+    <Container>
+      <Content>
+        <CTA>
+          <CTALogoOne src="/images/cta-logo-one.svg" alt="" />
+          <SignUp>GET PREMIER ACCESS NOW</SignUp>
+          <Description>
+          See Raya and the Last Dragon before it's available to all Disney+ subscribers on June 4. Watch as many times as you like with Premier Access for $34.99 and your Disney+ subscription.
+          </Description>
+          <CTALogoTwo src="/images/cta-logo-two.png" alt="" />
+        </CTA>
+        <BgImage />
+      </Content>
+    </Container>
+  );
 };
 
 const Container = styled.section`
@@ -21,15 +29,82 @@ const Container = styled.section`
 const Content = styled.div`
   margin-bottom: 10vw;
   width: 100%;
-  position: realtive;
+  position: relative;
   min-height: 100vh;
   box-sizing: border-box;
   display: flex;
   justify-content: center;
   align-items: center;
-  flex-dirextion: column;
+  flex-direction: column;
   padding: 80px 40px;
-  height: 100%
+  height: 100%;
+`;
+
+const BgImage = styled.div`
+  height: 100%;
+  background-position: top;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-image: url("/images/login-background.jpg");
+  position: absolute;
+  top: 0;
+  right: 0;
+  left: 0;
+  z-index: -1;
+`;
+
+const CTA = styled.div`
+    margin: 0px auto 2vw;
+    max-width: 650px;
+    flex-flow: column wrap;
+    display: flex;
+    -webkit-box-pack: center;
+    justify-content: center;
+    -webkit-box-align: center;
+    align-items: center;
+    text-align: center;
+    transition: opacity 0.2s ease 0s;
+    width: 100%;
+`;
+
+const CTALogoOne = styled.img`
+  margin-bottom: 12px;
+  max-width: 600px;
+  min-height: 1px;
+  display: block;
+  width: 100%;
+`;
+
+const SignUp = styled.a`
+  font-weight: bold;
+  color: #f9f9f9;
+  background-color: #0063e5;
+  margin-bottom: 12px;
+  width: 100%;
+  letter-spacing: 1.5px;
+  font-size: 18px;
+  padding: 16.5px 0;
+  border: 1px solid transparent;
+  border-radius: 4px;
+  &:hover {
+    background-color: #0483ee;
+  }
+`;
+
+const Description = styled.p`
+  color: hsla(0, 0%, 95.3%, 1);
+  font-size: 11px;
+  margin: 0 0 24px;
+  line-height: 1.5;
+  letter-spacing: 1.5px;
+`;
+
+const CTALogoTwo = styled.img`
+  max-width: 600px;
+  margin-bottom: 20px;
+  display: inline-block;
+  vertical-align: bottom;
+  width: 100%;
 `;
 
 export default Login;
