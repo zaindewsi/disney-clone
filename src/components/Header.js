1236805eeaa66cd4a18edf677e839089e9507551
Zain Dewsi <zain.dewsi@gmail.com>
2021-04-21 16:45:24 -0400

    added user states to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,43 @@
 import styled from "styled-components";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
 import { auth, provider } from "../firebase";
+import { selectUserName, selectUserPhoto, setUserLoginDetails} from "../features/user/userSlice"
 
 const Header = (props) => {
 
+  const dispatch = useDispatch()
+  const history = useHistory()
+  const userName = useSelector(selectUserName)
+  const userPhoto = useSelector(selectUserPhoto)
+
   const handleAuth = () => {
     auth.signInWithPopup(provider).then((result) => {
-      console.log(result)
+      setUser(result.user);
     }).catch((error) => {
       alert(error.message)
-    })
-  }
+    });
+  };
+
+  const setUser = (user) => {
+    dispatch(
+      setUserLoginDetails({
+        name: user.displayName,
+        email: user.email,
+        photo: user.photoURL,
+      })
+      );
+  };
 
   return (
   <Nav>
-    <Logo> <img src="images/logo.svg" alt="Disney logo"/></Logo>
+    <Logo> <img src="images/logo.svg" alt="Disney+"/></Logo>
+
+    {
+      !userName ? <Login onClick = {handleAuth}>Login</Login> : 
+      <> 
+      
+    
     <NavMenu>
     <a href="/home">
       <img src="/images/home-icon.svg" alt="HOME" />
@@ -40,7 +64,9 @@ const Header = (props) => {
       <span>SERIES</span>
     </a>
     </NavMenu>
-    <Login onClick = {handleAuth}>Login</Login>
+    <UserImg src = {userPhoto} alt = {userName} />
+    </>
+  }
   </Nav>
   )
 }
@@ -151,5 +177,8 @@ const Login = styled.a`
   }
 `;
 
+const UserImg = styled.img`
+  height: 100%;
+`;
 
 export default Header
