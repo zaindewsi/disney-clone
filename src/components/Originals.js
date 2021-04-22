d8c9ff0752f344cfd79f9ccebb6448ec1d581834
Zain Dewsi <zain.dewsi@gmail.com>
2021-04-22 02:01:59 -0400

    fixed mobile responsiveness

--- src/components/Originals.js
+++ src/components/Originals.js
@@ -1,30 +1,31 @@
 import styled from "styled-components";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
-import { selectOriginal } from "../features/movie/movieSlice"
+import { selectOriginal } from "../features/movie/movieSlice";
 
 const Originals = (props) => {
   const movies = useSelector(selectOriginal);
+
   return (
     <Container>
-    <h1>Disney+ Originals</h1>
+      <h4>Originals</h4>
       <Content>
-      {movies && movies.map((movie, key) => (
-          <Wrap key={key}>
-            {movies.id}
-            <Link to ={"/detail/" + movie.id}>
-              <img src={movie.cardImg} alt={movie.title} />
-            </Link>
-          </Wrap>
-        ))}
+        {movies &&
+          movies.map((movie, key) => (
+            <Wrap key={key}>
+              {movie.id}
+              <Link to={`/detail/` + movie.id}>
+                <img src={movie.cardImg} alt={movie.title} />
+              </Link>
+            </Wrap>
+          ))}
       </Content>
-      
     </Container>
-  )
-}
+  );
+};
 
 const Container = styled.div`
-  padding: 0 0 25px;
+  padding: 0 0 26px;
 `;
 
 const Content = styled.div`
@@ -32,40 +33,39 @@ const Content = styled.div`
   grid-gap: 25px;
   gap: 25px;
   grid-template-columns: repeat(4, minmax(0, 1fr));
-
   @media (max-width: 768px) {
     grid-template-columns: repeat(2, minmax(0, 1fr));
   }
 `;
 
 const Wrap = styled.div`
-  padding-top: 50%;
+  padding-top: 56.25%;
   border-radius: 10px;
-  box-shadow: rgb(0 0 0 / 69%) 0 26px 30px -10px, rgb(0 0 0 / 73%) 0 16px 10px -10px;
+  box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
+    rgb(0 0 0 / 73%) 0px 16px 10px -10px;
   cursor: pointer;
   overflow: hidden;
   position: relative;
-  transition: all 250ms cubic-bezier(0.25, 0.5, 0.45, 0.95);
-  border: 3px solid rgba(249, 294, 249, 0.1);
-
+  transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+  border: 3px solid rgba(249, 249, 249, 0.1);
   img {
     inset: 0px;
     display: block;
-    object-fit: cover;
     height: 100%;
-    width: 100%;
+    object-fit: cover;
     opacity: 1;
     position: absolute;
-    transition: opacity 500ms ease-in-out;
+    transition: opacity 500ms ease-in-out 0s;
+    width: 100%;
     z-index: 1;
+    top: 0;
   }
-
   &:hover {
-    box-shadow: rgb(0 0 0 / 69%) 0 26px 30px -10px, rgb(0 0 0 / 73%) 0 16px 10px -10px;
+    box-shadow: rgb(0 0 0 / 80%) 0px 40px 58px -16px,
+      rgb(0 0 0 / 72%) 0px 30px 22px -10px;
     transform: scale(1.05);
-    border-color: #fff;
-
+    border-color: rgba(249, 249, 249, 0.8);
   }
 `;
 
-export default Originals
+export default Originals;
